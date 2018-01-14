0a5f8d02893a7f7fd01b8211126caf9b6ff3efeb
Abid Hasan <abidhasan@gmail.com>
2018-01-13 21:18:15 -0500

    Metar parse function

--- scripts.js
+++ scripts.js
@@ -1,117 +1,139 @@
 function decode_metar(e) {
-  //To return
-  let parsedMetar = [];
+  const metar = this.value.split("=")[0].split("RMK")
 
-  //Grab the metar text - it's split by "=" sign
-  const text = this.value.split("=")[0];
+  const mainMetarText = metar[0]
+  const remarks = metar[1] || null
 
-  //Split into tokens
-  const tokens = text.split(" ");
+  let parsedMetar = [
+      {name: "Metar Type", pattern: /METAR|SPECI/, match: null},                                          //DONE
+      {name: "Airport Code", pattern: /([A-Z]{4})( AUTO)?( [A-Z]{3})?/, match: null},
+      {name: "Date", pattern: /\d{6}Z/, match: null},
+      {name: "Wind", pattern: /(\d{3}\d{1,2}G\d{1,2}KT)/, match: null},
+      {name: "Visibility", pattern: /(\d{1,2}\/?\d{1,2}SM)/, match: null},
+      {name: "Runway Visibility", pattern: /R\d{2}\/\d{4}FT(\/[DUN])?/, match: null},
+      {name: "Weather Description", pattern: /([-+]?(MI|BC|PR|DR|BL|SH|TS|FZ)?(DZ|RA|SN|SG|IC|PL|GR|GS|UP))/, match: null},
+      {name: "Clouds", pattern: /(SKC|FEW|SCT|BKN|OVC|VV)\d{3}/, match: null},
+      {name: "Temperature", pattern: /M?\d{1,2}\/M?\d{1,2}/, match: null},
+      {name: "Pressure", pattern: /[AQ]\d{4}/, match: null},
+      {name: "Windshear", pattern: /WS ((RWY\d{2})|(ALLRWY))/, match: null},
+  ]
 
-  // #1 METAR TYPE
-  let type;
-  //Define known metars
-  const metarTypes = {"SPECI": "Special Report", "METAR": "Regular Metar Report"};
+  console.clear()
 
-  tokens.forEach(key => {
-    if (key in metarTypes) type = metarTypes[key]
-  });
-  type = type || "Unknown Report Type"
-
-          if (type) console.log(type)         //////////// DEVELOPMENTAL
-
-  Object.keys(metarTypes).forEach((key) => {
-      //check if key in metar
-
-  })
-//  tokens.find()
-
+  for (let metar in parsedMetar) {
+    console.log("\n")
+    console.log(parsedMetar[metar].pattern)
+    console.log(mainMetarText.match(parsedMetar[metar].pattern))
+  }
 
 }
 
-//     // Check if initial token is non-METAR date
-//     var reDate = /^\d\d\d\d\/\d\d\/\d\d/;
-//     if (reDate.test(arrayOfTokens[numToken]))
-//         numToken++;
+
 //
-//     // Check if initial token is non-METAR time
-//     var reTime = /^\d\d:\d\d/;
-//     if (reTime.test(arrayOfTokens[numToken]))
-//         numToken++;
+//   //To return
+//   let parsedMetar = [];
 //
-//     // Check if initial token indicates type of report
-//     if(arrayOfTokens[numToken] == "METAR")
-//         numToken++;
-//     else if(arrayOfTokens[numToken] == "SPECI")
-//     {
-//         add_output("Report is a SPECIAL report\n");
-//         numToken++;
+//   //Grab the metar text - it's split by "=" sign
+//   const text = this.value.split("=")[0];
+//   const tokens = text.split(" ");
+//
+//   //Build parser
+//   const parser = [
+//     {"Special Report": "SPECI", "Regular Metar Report": "METAR"},
+//     {"Date": /^\d\d\d\d\d\d[Z]$/},
+//     {}
+//
+//
+//   ]
+//
+//   // #1 METAR TYPE
+//   let type;
+//   //Define known metars
+//   const metarTypes = ;
+//   const reDate = ;
+//   const wxStation = /^[A-Z]{4}$/
+//   //Find these items
+//
+//   text.split(" ").forEach((key, index) => {
+//     //console.log("METAR" in Object.keys(metarTypes))
+//     if (Object.keys(metarTypes).includes(key)) {
+//       console.log('report matched  ' + key) //type = metarTypes[key]
+//       return
 //     }
-//
-//     // Parse location token
-//     if (arrayOfTokens[numToken].length == 4)
-//     {
-//         add_output("Location: " + arrayOfTokens[numToken] + "\n");
-//         numToken++;
+//     if (reDate.test(key)) {
+//       console.log('date matche  ' + key) //type = metarTypes[key]
+//       return
 //     }
-//     else
-//     {
-//         add_output("Invalid report: malformed location token '" + arrayOfTokens[numToken] + "' \n-- it should be 4 characters long!");
-//         return;
-//     }
-//
-//
-//     // Parse date-time token -- we allow time specifications without final 'Z'
-//     if ( (
-//            ( (arrayOfTokens[numToken].length == 7) &&
-//              (arrayOfTokens[numToken].charAt(6) == 'Z') ) ||
-//            ( arrayOfTokens[numToken].length == 6 )
-//          ) &&
-//          is_num_digit(arrayOfTokens[numToken].charAt(0)) &&
-//          is_num_digit(arrayOfTokens[numToken].charAt(1)) &&
-//          is_num_digit(arrayOfTokens[numToken].charAt(2)) &&
-//          is_num_digit(arrayOfTokens[numToken].charAt(3)) &&
-//          is_num_digit(arrayOfTokens[numToken].charAt(4)) &&
-//          is_num_digit(arrayOfTokens[numToken].charAt(5))    )
-//     {
-//         add_output("Day of month: " + arrayOfTokens[numToken].substr(0,2) + "\n");
-//         add_output("Time: " + arrayOfTokens[numToken].substr(2,2) +":" +
-//                               arrayOfTokens[numToken].substr(4,2) + " UTC");
-//
-//         if(arrayOfTokens[numToken].length == 6)
-//             add_output(" (Time specification is non-compliant!)");
-//
-//         add_output("\n");
-//         numToken++;
-//     }
-//     else
-//     {
-//         add_output("Time token not found or with wrong format!");
-//         return;
+//     if (wxStation.test(key)) {
+//       console.log('weather station matched  ' + key) //type = metarTypes[key]
+//       return
 //     }
 //
 //
-//     // Check if "AUTO" or "COR" token comes next.
-//     if (arrayOfTokens[numToken] == "AUTO")
-//     {
-//         add_output("Report is fully automated, with no human intervention or oversight\n");
-//         numToken++;
-//     }
-//     else if (arrayOfTokens[numToken] == "COR")
-//     {
-//         add_output("Report is a correction over a METAR or SPECI report\n");
-//         numToken++;
-//     }
+//   });
 //
-//     // Parse remaining tokens
-//     for (var i=numToken; i<arrayOfTokens.length; i++)
-//     {
-//         if(arrayOfTokens[i].length > 0)
-//         {
-//             decode_token(arrayOfTokens[i].toUpperCase());
-//         }
-//         else
-//         {
-//             add_output("Next token has 0 length\n");
-//         }
-//     }
+//   console.log(tokens)
+// //  type = type || "Unknown Report Type"
+//
+//           if (type) console.log(type)         //////////// DEVELOPMENTAL
+//
+//   //Find date
+//
+//
+// }
+//
+// //
+// //     // Parse date-time token -- we allow time specifications without final 'Z'
+// //     if ( (
+// //            ( (arrayOfTokens[numToken].length == 7) &&
+// //              (arrayOfTokens[numToken].charAt(6) == 'Z') ) ||
+// //            ( arrayOfTokens[numToken].length == 6 )
+// //          ) &&
+// //          is_num_digit(arrayOfTokens[numToken].charAt(0)) &&
+// //          is_num_digit(arrayOfTokens[numToken].charAt(1)) &&
+// //          is_num_digit(arrayOfTokens[numToken].charAt(2)) &&
+// //          is_num_digit(arrayOfTokens[numToken].charAt(3)) &&
+// //          is_num_digit(arrayOfTokens[numToken].charAt(4)) &&
+// //          is_num_digit(arrayOfTokens[numToken].charAt(5))    )
+// //     {
+// //         add_output("Day of month: " + arrayOfTokens[numToken].substr(0,2) + "\n");
+// //         add_output("Time: " + arrayOfTokens[numToken].substr(2,2) +":" +
+// //                               arrayOfTokens[numToken].substr(4,2) + " UTC");
+// //
+// //         if(arrayOfTokens[numToken].length == 6)
+// //             add_output(" (Time specification is non-compliant!)");
+// //
+// //         add_output("\n");
+// //         numToken++;
+// //     }
+// //     else
+// //     {
+// //         add_output("Time token not found or with wrong format!");
+// //         return;
+// //     }
+// //
+// //
+// //     // Check if "AUTO" or "COR" token comes next.
+// //     if (arrayOfTokens[numToken] == "AUTO")
+// //     {
+// //         add_output("Report is fully automated, with no human intervention or oversight\n");
+// //         numToken++;
+// //     }
+// //     else if (arrayOfTokens[numToken] == "COR")
+// //     {
+// //         add_output("Report is a correction over a METAR or SPECI report\n");
+// //         numToken++;
+// //     }
+// //
+// //     // Parse remaining tokens
+// //     for (var i=numToken; i<arrayOfTokens.length; i++)
+// //     {
+// //         if(arrayOfTokens[i].length > 0)
+// //         {
+// //             decode_token(arrayOfTokens[i].toUpperCase());
+// //         }
+// //         else
+// //         {
+// //             add_output("Next token has 0 length\n");
+// //         }
+// //     }
